137c6178591976454adf554ebf7a1a5e16ec2ee3
Yulia Semenyuk <semenyukyp@gmail.com>
2022-11-21 21:39:28 +0200

    contactsSlice refactored

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./components/App";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <App />
-      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
